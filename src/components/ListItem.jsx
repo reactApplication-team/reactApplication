fe42c1b2255e492d9d8c982a91ac3ced59d3b497
paujorbaa <pau.jorbaa@gmail.com>
2025-10-03 17:59:04 +0200

    add items to sidebar cart implemented

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,38 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import axios from "axios" 
-import { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useCart } from "../context/CartContext";
 
 const ListItem = () => {
-  const [itemCard,setItemCard]=useState([]);
-  
-  useEffect(()=>{
-  const getItems = async () => {
-    try {
-      const {data} = await axios.get('https://dummyjson.com/products/?limit=190')
-      console.log(data)
-      setItemCard(data.products)
-    } catch (error) {
-      console.log(error) 
-    }};
-    getItems();
-  },[])
-   
-  return (
-    <div >
-      {
-        itemCard.map(item=>(
-          <div key={item.id}>
-            <h3>{item.title}</h3>
-            <p>${item.price}</p>
-            <img src={item.thumbnail} alt={itemCard.title} />
-          </div>
-        )
-        )
-      }
-      
-    </div>
-  )
-}
+  const [itemCard, setItemCard] = useState([]);
+  const { addToCart } = useCart();
 
-export default ListItem
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://dummyjson.com/products/?limit=190"
+        );
+        setItemCard(data.products);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getItems();
+  }, []);
+
+  return (
+    <div>
+      {itemCard.map((item) => (
+        <div key={item.id}>
+          <h3>{item.title}</h3>
+          <p>${item.price}</p>
+          <img src={item.thumbnail} alt={item.title} />
+          <button onClick={() => addToCart(item)}>Add to cart</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListItem;
