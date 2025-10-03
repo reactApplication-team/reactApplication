afd0c99e847c6cbbc72ded8a458a0b7a2603fada
Yanqing Yang <yang.yanqing@tutamail.com>
2025-10-03 15:11:03 +0200

    The Homepage that contains all the Items

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import "../src/styles/App.css";
 import Footer from "./components/Footer";
+import ListItem from "./components/ListItem";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <Navbar toggleSidebar={toggleSidebar} />
+      <ListItem />
       <Sidebar isOpen={isSidebarOpen} />
       <Footer />
       <h1>Main Content</h1>
