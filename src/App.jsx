dd4c007cb6f33cdba93e2bc586d1949b2c293cb6
Yanqing Yang <yang.yanqing@tutamail.com>
2025-10-09 12:28:09 +0200

    Chang of cart5

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import DashboardPage from "./pages/DashboardPage";
 import ItemsDetailsPage from "./pages/ItemsDetailsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import TagPage from "./pages/TagsPage";
-import Cart from "./components/cart";
+import CartComponent from "./components/CartComponent";
 import CreateItemPage from "../src/pages/CreateItemPage"
 import UpdateItemPage from "../src/pages/UpdateItemPage"
 import "../src/styles/App.css";
@@ -43,7 +43,7 @@ export default function App() {
         <Route path="/items/:itemId/edit" element={<UpdateItemPage />} /> 
         <Route path="/" element={<DashboardPage />} />
         <Route path="items/:itemId" element={<ItemsDetailsPage />} />
-        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Cart" element={<CartComponent />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/tags/:tag" element={<TagPage />} />
       </Route>
