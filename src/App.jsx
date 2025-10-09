45d80c1864c0d0e2a78e63305527322e81c09425
Yanqing Yang <yang.yanqing@tutamail.com>
2025-10-09 12:11:32 +0200

    Chang of cart

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
         <Route path="/items/:itemId/edit" element={<UpdateItemPage />} /> 
         <Route path="/" element={<DashboardPage />} />
         <Route path="items/:itemId" element={<ItemsDetailsPage />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/Cart" element={<Cart />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/tags/:tag" element={<TagPage />} />
       </Route>
